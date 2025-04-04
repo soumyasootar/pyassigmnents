02d856504d389d8abd567f871205107651b6207c
soumyasootar <soumyaswaroopsootar@gmail.com>
2025-04-04 06:35:08 +0530

    First Assignment

---
+++ Assignment 1 Basic Python Concepts/Task 1.py
@@ -0,0 +1,10 @@
+# Task 1: Perform Basic Mathematical Operations
+
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+
+print('\n')
+print('Addition', int(num1 + num2))
+print('Subtraction', int(num1 - num2))
+print('Multiplication', int(num1 * num2))
+print('Division',round(num1 / num2 ,2))
---
+++ Assignment 1 Basic Python Concepts/Task 2.py
@@ -0,0 +1,6 @@
+# Task 2: Create a Personalized Greeting
+
+firstName = input("Enter your first name : ")
+lastName = input("Enter your last name : ")
+
+print('Hello, ' + firstName + " " + lastName + '! Welcome the Python program.')
