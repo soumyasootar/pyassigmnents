1ace182552379c51843ce1ca925a5f3c4529cc73
soumyasootar <soumyaswaroopsootar@gmail.com>
2025-04-26 18:53:26 +0530

    2nd Assignment

---
+++ ASSIGNMENT 2 Control Structures in Python/Task 1.py
@@ -0,0 +1,5 @@
+# Task 1: Check if a Number is Even or Odd
+
+num = int(input("Enter a number: "))
+print('\n')
+print(num, "is an", "even" if num % 2 == 0 else "odd", "number")
---
+++ ASSIGNMENT 2 Control Structures in Python/Task 2.py
@@ -0,0 +1,8 @@
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+start=1
+end=50
+total =0
+for num in range(start,end+1):
+    total+=num
+print("The sum of numbers from 1 to 50 is:", total)
